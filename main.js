bb88868edffb1a7092bd8c60892b152786655326
DEMUK_ <denysmutay@gmail.com>
2020-12-19 12:14:50 +0300

    changed function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Dank Farik')
+// take user size of leg as a Number
+
+function ShoeSize() {
+    var MyShoeSize = document.getElementById("lengthOfFoot").value;
+
+    document.getElementById("shoesize").innerHTML = MyShoeSize;
+
+    // console.log(MyShoeSize)
+}
+//let lengthOfLeg = Number(prompt('What is the length of your leg'))
+
+//console.log('The size of your leg is ' + lengthOfLeg)
