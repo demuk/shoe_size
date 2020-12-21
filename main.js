f3e10f491f14187a8747f0353e8a13e9f2c64ac3
DEMUK_ <denysmutay@gmail.com>
2020-12-21 19:12:18 +0300

    added conditionals

--- main.js
+++ main.js
@@ -1,12 +1,89 @@
 // take user size of leg as a Number
 
-function ShoeSize() {
-    var MyShoeSize = document.getElementById("lengthOfFoot").value;
+function checkTheSizeOfTheShoe() {
+    let sizeOfFoot = document.getElementById("lengthOfFoot").value;
 
-    document.getElementById("shoesize").innerHTML = MyShoeSize;
+    if (sizeOfFoot == 23.5) {
+        convertedSize = 6;
 
-    // console.log(MyShoeSize)
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+
+    else if (sizeOfFoot == 24.1) {
+        convertedSize = 6.5;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+
+    else if (sizeOfFoot == 24.4) {
+        convertedSize = 7;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 24.8) {
+        convertedSize = 7.5;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 25.4) {
+        convertedSize = 8;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 25.7) {
+        convertedSize = 8.5;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 26) {
+        convertedSize = 9;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 26.7) {
+        convertedSize = 9.5;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 27) {
+        convertedSize = 10;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 27.3) {
+        convertedSize = 10.5;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 27.9) {
+        convertedSize = 11;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 28.3) {
+        convertedSize = 11.5;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 28.6) {
+        convertedSize = 12;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 29.4) {
+        convertedSize = 13;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else if (sizeOfFoot == 30.2) {
+        convertedSize = 14;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    else (sizeOfFoot == 31); {
+        convertedSize = 15;
+
+        document.getElementById("shoesize").innerHTML = convertedSize;
+    }
+    // document.getElementById("shoesize").innerHTML = sizeOfFoot;
 }
-//let lengthOfLeg = Number(prompt('What is the length of your leg'))
-
-//console.log('The size of your leg is ' + lengthOfLeg)
