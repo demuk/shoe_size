73807b84602b3cad0eb394631d12cd391a0a8e2c
Kiptoo <denysmutay@gmail.com>
2020-12-22 10:42:55 +0300

    refactored the code

--- main.js
+++ main.js
@@ -1,89 +1,16 @@
 // take user size of leg as a Number
 
 function checkTheSizeOfTheShoe() {
-    let sizeOfFoot = document.getElementById("lengthOfFoot").value;
+    let sizeOfFoot = parseFloat(document.getElementById("lengthOfFoot").value, 10);
 
-    if (sizeOfFoot == 23.5) {
-        convertedSize = 6;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-
-    else if (sizeOfFoot == 24.1) {
-        convertedSize = 6.5;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-
-    else if (sizeOfFoot == 24.4) {
-        convertedSize = 7;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 24.8) {
-        convertedSize = 7.5;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 25.4) {
-        convertedSize = 8;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 25.7) {
-        convertedSize = 8.5;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 26) {
-        convertedSize = 9;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 26.7) {
-        convertedSize = 9.5;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 27) {
-        convertedSize = 10;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 27.3) {
-        convertedSize = 10.5;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 27.9) {
-        convertedSize = 11;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 28.3) {
-        convertedSize = 11.5;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 28.6) {
-        convertedSize = 12;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 29.4) {
-        convertedSize = 13;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else if (sizeOfFoot == 30.2) {
-        convertedSize = 14;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    else (sizeOfFoot == 31); {
-        convertedSize = 15;
-
-        document.getElementById("shoesize").innerHTML = convertedSize;
-    }
-    // document.getElementById("shoesize").innerHTML = sizeOfFoot;
+    let inputSize = [23.5, 24.1, 24.4, 24.8, 25.4, 25.7, 26, 26.7, 27, 27.3, 27.9, 28.3, 28.6, 29.4, 30.2, 31, 31.8 ];
+  
+  const outputSize = [6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 13, 14, 15 , 16];
+  
+  let result = 'Invalid Input Size';
+  if(inputSize.includes(sizeOfFoot)) {
+    result = outputSize[inputSize.indexOf(sizeOfFoot)]
+  }
+  
+    document.getElementById("shoesize").innerHTML = result;
 }
